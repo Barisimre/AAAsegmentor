71230d8b5ab435668ecf4013669fe0902c9b3970
Baris Imre <barisimre98@gmail.com>
2023-04-18 16:42:39 +0200

    Make 0.1 of my model in this repo

--- src/model/vit.py
+++ src/model/vit.py
@@ -1,16 +1,18 @@
+import einops
 import torch.nn as nn
-from einops import rearrange
 from src.model.blocks import TransformerBlock
 import torch
 
 
-class ViT(nn.Module):
+# TODO: add positional encoding
+# Untouched ViT from MONAI, used as an internal class
+class DefaultViT(nn.Module):
 
     def __init__(
             self,
-            hidden_size: int = 512,
-            mlp_dim: int = 512,
-            num_layers: int = 6,
+            hidden_size: int = 128,
+            mlp_dim: int = 256,
+            num_layers: int = 12,
             num_heads: int = 8,
             dropout_rate: float = 0.1,
             qkv_bias: bool = False,
@@ -25,55 +27,57 @@ class ViT(nn.Module):
 
         self.blocks = nn.ModuleList(
             [TransformerBlock(hidden_size, mlp_dim, num_heads,
-                              dropout_rate, qkv_bias) for i in range(num_layers)]
+                              dropout_rate, qkv_bias) for _ in range(num_layers)]
         )
         self.norm = nn.LayerNorm(hidden_size)
 
     def forward(self, x):
+        # TODO: positional encoding
 
         # attns = []
 
         for blk in self.blocks:
             x = blk(x)
+            # TODO
             # attns.append(a)
         x = self.norm(x)
         return x
 
 
-# TODO: add positional encoding
-class MyVit(nn.Module):
+class ViT(nn.Module):
 
-    def __init__(self, embed_dim, patch_size, num_layers=6, mlp_dim=256, levels=3, in_channels=8):
+    def __init__(self, embed_dim, patch_size, channels=8, levels=3):
         super().__init__()
-        self.levels = levels
-        self.pooling = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.embedders = nn.ModuleList(
-            [nn.Conv3d(kernel_size=patch_size, stride=patch_size, in_channels=1, out_channels=embed_dim) for _ in
-             range(levels)])
 
-        self.deembedders = nn.ModuleList(
-            [nn.ConvTranspose3d(kernel_size=patch_size, stride=patch_size, in_channels=embed_dim, out_channels=1) for _
-             in range(levels)])
+        self.patch_size = patch_size
 
-        self.vit = ViT(hidden_size=embed_dim, num_layers=num_layers, mlp_dim=mlp_dim)
+        self.embedders = nn.ModuleList([
+            nn.Conv3d(kernel_size=patch_size, stride=patch_size, in_channels=channels, out_channels=embed_dim)
+            for _ in range(levels)])
 
-    # input xs --> [(b c x y z), (b c x/2 y/2 z/2) ... ]
-    def forward(self, x):
-        # TODO
-        # split_points = [a.shape[]]
-        # all_tokens = torch.concat(x, dim=1)
-        # out_tokens = self.vit(all_tokens)
-        # xs = []
-        #
-        # level_sperated = torch.split(out_tokens, split_points, dim=1)
-        #
-        # for i in range(self.levels):
-        #     l = level_sperated[i]
-        #     side_len = round(l.shape[1] ** (1. / 3.))
-        #     ll = rearrange(l, "c (x y z) em -> c em x y z", x=side_len, y=side_len, z=side_len)
-        #
-        #     ll = self.deembedders[i](ll)
-        #     xs.append(ll)
-        #
-        # return xs
-        return None
+        self.de_embedders = nn.ModuleList([
+            nn.ConvTranspose3d(kernel_size=patch_size, stride=patch_size, in_channels=embed_dim, out_channels=channels)
+            for _ in range(levels)])
+
+        self.vit = DefaultViT(hidden_size=embed_dim, mlp_dim=embed_dim * 2)
+
+    # xs: one x per level of the encoder. They should all halve in every size. Channel counts don't matter.
+    def forward(self, xs):
+        shapes = [int(x.shape[-1] / self.patch_size) for x in xs]
+
+        # Embed
+        xs = [einops.rearrange(em(x), "b em x y z -> b (x y z) em") for x, em in zip(xs, self.embedders)]
+
+        token_counts = [x.shape[1] for x in xs]
+
+        # TODO: positional encoding
+
+        # ViT
+        xs = self.vit(torch.cat(xs, dim=1))
+        xs = torch.split(xs, token_counts, dim=1)
+
+        # De-Embed
+        xs = [einops.rearrange(x, "b (x y z) em -> b em x y z", x=s, y=s, z=s) for x, s in zip(xs, shapes)]
+        xs = [dem(x) for x, dem in zip(xs, self.de_embedders)]
+
+        return xs
