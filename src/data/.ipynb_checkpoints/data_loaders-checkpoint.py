045e475f023b169300ce7284b4bd488bedf2e209
b.imre@student.utwente.nl <s1797743@hpc-head1.local>
2023-04-20 12:43:34 +0200

    Cluster changes

---
+++ src/data/.ipynb_checkpoints/data_loaders-checkpoint.py
@@ -0,0 +1,18 @@
+from src.constants import *
+from src.data.transforms import train_transform, test_transform
+import os
+
+
+def get_loaders():
+
+    filenames = os.listdir(DATA_PATH + "/images")
+    filenames = [f for f in filenames if 'mhd' in f]
+    train_dataset = monai.data.CacheDataset(filenames, transform=train_transform, num_workers=16)
+    train_loader = monai.data.DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    filenames = os.listdir(DATA_PATH + "/test_images")
+    filenames = [f for f in filenames if 'mhd' in f]
+    test_dataset = monai.data.CacheDataset(filenames, transform=test_transform, num_workers=16)
+    test_loader = monai.data.DataLoader(test_dataset, batch_size=1, shuffle=True)
+
+    return train_loader, test_loader
---
+++ src/training/.ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,53 @@
+from src.constants import *
+from src.utils.metrics import dice_scores
+import numpy as np
+import wandb
+from src.utils.visualisations import visualize_sample_to_wandb
+
+
+def test_single_epoch(model, test_loader):
+    model.eval()
+    scores = []
+
+    largest_component = monai.transforms.KeepLargestConnectedComponent()
+
+    visualised = False
+
+    with torch.no_grad():
+        for d in test_loader:
+            img = d['img'].to(DEVICE)
+            mask = d['mask'].to(DEVICE)
+
+            out = monai.inferers.sliding_window_inference(img,
+                                                          roi_size=CROP_SIZE,
+                                                          sw_batch_size=BATCH_SIZE,
+                                                          predictor=model,
+                                                          overlap=0.5,
+                                                          sw_device=DEVICE,
+                                                          device="cpu",
+                                                          progress=False,
+                                                          )
+            out = torch.argmax(out, 1, keepdim=True)
+            out = largest_component(out)
+            s = dice_scores(out, mask)
+            scores.append(s)
+
+            # Send one sample to wandb
+            if not visualised:
+                visualize_sample_to_wandb(img, mask, out, s)
+                visualised = True
+
+        scores = np.array(scores)
+        scores = np.nan_to_num(scores, copy=True, nan=1.0)
+        scores = np.sum(scores, axis=0) / scores.shape[0]
+
+    test_score = np.sum(scores) / (len(scores) * 1.0)
+
+    wandb.log({"test_score": test_score,
+               "background": scores[0],
+               "lumen": scores[1],
+               "thrombus": scores[2]})
+
+    model.train()
+
+    return test_score
