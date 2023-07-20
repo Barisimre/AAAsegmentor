c2cfb5b600686e8d8c3eee76772c966ee97e9c83
Baris Imre <barisimre98@gmail.com>
2023-07-20 12:36:49 +0200

    Change constants

--- src/constants.py
+++ src/constants.py
@@ -16,7 +16,7 @@ MODEL_SAVE_PATH = f"{GENERAL_PATH}/models"
 RESULTS_SAVE_PATH = f"{GENERAL_PATH}/results"
 
 TEST_PATIENTS = [10, 20, 31, 42, 55, 82]
-DATA_LIMIT = 10
+DATA_LIMIT = -1
 
 # Sizes and transformations
 CROP_SIZE = (256, 256, 64)
@@ -33,7 +33,7 @@ DEVICE = "cuda"
 LEARNING_RATES = {0: 3e-4, 1000: 1e-4, 1750: 7e-5, 2000: 2e-5}
 
 NUM_EPOCHS = 2500
-BATCH_SIZE = 1
+BATCH_SIZE = 4
 LOSS = monai.losses.DiceCELoss(lambda_ce=0.4)
 
 # Transformer stuff
